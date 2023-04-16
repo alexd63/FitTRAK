20d5a20f02ec9541ba9b50fde08de6fba32d624d
alexd63 <ajdiaz5800@gmail.com>
2023-04-16 11:52:45 -0700

    Initial Commit

---
+++ FitTRAK/FitTRAKApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitTRAKApp.swift
+//  FitTRAK
+//
+//  Created by Alex Diaz on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FitTRAKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
